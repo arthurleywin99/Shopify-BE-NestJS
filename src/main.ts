b338aa7e8ed6d1266dea5544e1df52952497e05a
Giang Bui <buinguyentruonggiang.1112@gmail.com>
2025-06-13 11:56:37 +0700

    [Feature]: Init default file and structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder()
+    .setTitle('Shopify')
+    .setDescription('The shopify APIs description')
+    .setVersion('1.0')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
+  app.setGlobalPrefix('/api/v1')
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  )
+  app.enableCors({
+    origin: [process.env.FRONTEND_URL],
+  })
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+
+bootstrap()
